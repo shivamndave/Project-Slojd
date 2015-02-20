d10d528e6e274532fa2b913e41167df4cf490bf7
Andy Ly <andyly25@yahoo.com>
2015-02-19 22:25:22 -0800

    testing python out

---
+++ testing.py
@@ -0,0 +1,53 @@
+from random import randint
+import sys
+import glob
+import errno
+import os
+
+
+wordList1 = ["Enchanting", "Amazing", "Colourful", "Delightful", "Delicate"]
+wordList2 = ["visions", "distance", "conscience", "process", "chaos"]
+wordList3 = ["superstitious", "contrasting", "graceful", "inviting", "contradicting", "overwhelming"]
+wordList4 = ["true", "dark", "cold", "warm", "great"]
+wordList5 = ["scenery","season", "colours","lights","Spring","Winter","Summer","Autumn"]
+wordList6 = ["undeniable", "beautiful", "irreplaceable", "unbelievable", "irrevocable"]
+wordList7 = ["inspiration", "imagination", "wisdom", "thoughts"]
+		
+  
+# path = '/home/andy/Desktop/School/cmps112/projectslojd/dict/nouns/4syllablenouns.txt'   
+# This path2 seems to work
+path2 = os.path.dirname(os.path.abspath(__file__)) + "/dict/nouns/1syllablenouns.txt"
+
+print path2
+
+text_file = open(path2, "r")
+# lines = text_file.readlines()
+lines = text_file.read().split('\r\n')
+lines = filter(None, lines)
+print lines
+print len(lines)
+text_file.close()
+
+# # This thing just prints out the contents of the file
+# files = glob.glob(path2)   
+# for name in files: # 'file' is a builtin type, 'name' is a less-ambiguous variable name.
+#     try:
+#         with open(name) as f: # No need to specify 'r': this is the default.
+#             sys.stdout.write(f.read())
+#     except IOError as exc:
+#         if exc.errno != errno.EISDIR: # Do not fail if a directory is found, just ignore it.
+#             raise # Propagate other kinds of IOError.
+
+wordIndex1=randint(0, len(wordList1)-1)
+wordIndex2=randint(0, len(wordList2)-1)
+wordIndex3=randint(0, len(wordList3)-1)
+wordIndex4=randint(0, len(wordList4)-1)
+wordIndex5=randint(0, len(wordList5)-1)
+wordIndex6=randint(0, len(wordList6)-1)
+wordIndex7=randint(0, len(wordList7)-1)
+
+haiku = wordList1[wordIndex1] + " " + wordList2[wordIndex2] + ",\n" 
+haiku = haiku + wordList3[wordIndex3] + " " + wordList4[wordIndex4] + " " + wordList5[wordIndex5]  + ",\n"
+haiku = haiku + wordList6[wordIndex6] + " " + wordList7[wordIndex7] + "."
+
+print(haiku)
