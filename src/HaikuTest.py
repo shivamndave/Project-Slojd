4f048dae9e47211f208e49b519f3ed63e29e39d4
Shivam Dave <sdave@ucsc.edu>
2015-03-07 14:50:23 -0800

    MOdifies HaikuMod.py to be better looking/efficient and puts everything into a function

--- src/HaikuTest.py
+++ src/HaikuTest.py
@@ -47,35 +47,3 @@ def makeLine1(sylCountList):
 
 makeHaiku1()
 
-
-
-# ranSylNum = randint(1, 4)
-# ranFileNum = randint(1, 4)
-# ranLines = HaikuMod.getWordFile(ranFileNum, ranSylNum)
-# randNounNum = randint(0, len(ranLines)-1)
-# print ("Random " + str(ranFileNum) + " List(" + str(ranSylNum) + ") " + ranLines[randNounNum])
-
-# Each of these gets a random file from each type and 
-# puts it into a variable
-# randomNounSylNum = randint(1, 4)
-# randomNounLines = HaikuMod.getNounFile(randomNounSylNum)
-# randNounNum = randint(0, len(randomNounLines)-1)
-
-# randomAdjSylNum = randint(1, 4)
-# randomAdjLines = HaikuMod.getAdjFile(randomAdjSylNum)
-# randAdjNum = randint(0, len(randomAdjLines)-1)
-
-# randomVerbSylNum = randint(1, 4)
-# randomVerbLines = HaikuMod.getVerbFile(randomVerbSylNum)
-# randVerbNum = randint(0, len(randomVerbLines)-1)
-
-# randomAdverbSylNum = randint(1, 4)
-# randomAdverbLines = HaikuMod.getAdverbFile(randomAdverbSylNum)
-# randAdverbNum = randint(0, len(randomAdverbLines)-1)
-
-# Shows which random syllable list is returned from each type
-# print ("Random Noun List(" + str(randomNounSylNum) + ") " + randomNounLines[randNounNum])
-# print ("Random Adj List(" + str(randomAdjSylNum) + ") " + randomAdjLines[randAdjNum])
-# print ("Random Verb List(" + str(randomVerbSylNum) + ") " + randomVerbLines[randVerbNum])
-# print ("Random Adverb List(" + str(randomAdverbSylNum) + ") " + randomAdverbLines[randAdverbNum])
-
